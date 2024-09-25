f7edf951f6aab5e959e2ce2e6b202a7713c375c9
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-09-25 11:40:36 -0500

    Update 2_Detección de Enfermedades.py

--- pages/2_Detección de Enfermedades.py
+++ pages/2_Detección de Enfermedades.py
@@ -16,29 +16,30 @@ bg_image = st.file_uploader("Cargar Imagen:", type=["png", "jpg"])
 if bg_image is not None:
     uploaded_file=bg_image
     st.image(uploaded_file, caption='Imagen cargada.', use_column_width=True)
+try:
 
-
-if img_file_buffer is not None  or uploaded_file is not None:
-    # To read image file buffer with OpenCV:
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-   #To read image file buffer as a PIL Image:
-    img = Image.open(bg_image)   #img_file_buffer
-
-    newsize = (224, 224)
-    img = img.resize(newsize)
-    # To convert PIL Image to numpy array:
-    img_array = np.array(img)
-
-    # Normalize the image
-    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model.predict(data)
-    print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Saludable, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('Con Semptoria, con Probabilidad: '+str( prediction[0][1]))
-
+    if img_file_buffer is not None  or uploaded_file is not None:
+        # To read image file buffer with OpenCV:
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+       #To read image file buffer as a PIL Image:
+        img = Image.open(bg_image)   #img_file_buffer
+    
+        newsize = (224, 224)
+        img = img.resize(newsize)
+        # To convert PIL Image to numpy array:
+        img_array = np.array(img)
+    
+        # Normalize the image
+        normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
+        # Load the image into the array
+        data[0] = normalized_image_array
+    
+        # run the inference
+        prediction = model.predict(data)
+        print(prediction)
+        if prediction[0][0]>0.5:
+          st.header('Saludable, con Probabilidad: '+str( prediction[0][0]) )
+        if prediction[0][1]>0.5:
+          st.header('Con Semptoria, con Probabilidad: '+str( prediction[0][1]))
+  except:
+        pass
